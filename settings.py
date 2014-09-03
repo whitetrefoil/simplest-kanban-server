92fd62bf5431ececd32739b129c52782064ec914
WhiteTrefoil <whitetrefoil@gmail.com>
2014-09-03 22:26:27 +0800

    Eve files

---
+++ settings.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+DEBUG = True
+
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_DBNAME = 'simplest-kanban-eve'
+
+RESOURCE_METHODS = ['GET', 'POST']
+ITEM_METHODS = ['GET', 'PUT', 'DELETE']
+BANDWIDTH_SAVER = False
+
+ALLOWED_FILTERS = []
+PROJECTION = False
+
+X_DOMAINS = '*'
+
+tasks = {
+    'schema': {
+        'name': {
+            'type': 'string',
+            'minlength': 1,
+            'required': True
+        },
+        'assignee': {
+            'type': 'string',
+            'nullable': True,
+            'default': None,
+            'data_relation': {
+                'resource': 'assignees',
+                'field': 'name'
+            }
+        },
+        'status': {
+            'type': 'string',
+            'required': True,
+            'data_relation': {
+                'resource': 'statuses',
+                'field': 'code'
+            }
+        },
+        'milestone': {
+            'type': 'objectid',
+            'nullable': True,
+            'default': None,
+            'data_relation': {
+                'resource': 'milestones',
+                'field': '_id',
+                'embeddable': True
+            }
+        }
+    }
+}
+
+assignees = {
+    'schema': {
+        'name': {
+            'type': 'string',
+            'required': True
+        }
+    }
+}
+
+statuses = {
+    'item_title': 'status',
+    'schema': {
+        'code': {
+            'type': 'string',
+            'required': True
+        },
+        'name': {
+            'type': 'string',
+            'required': True
+        },
+        'cssClass': {
+            'type': 'string',
+            'default': ''
+        }
+    }
+}
+
+milestones = {
+    'schema': {
+        'name': {
+            'type': 'string',
+            'required': True
+        },
+        'isClosed': {
+            'type': 'boolean',
+            'default': False
+        }
+    }
+}
+
+DOMAIN = {
+    'tasks': tasks,
+    'assignees': assignees,
+    'statuses': statuses,
+    'milestones': milestones
+}
